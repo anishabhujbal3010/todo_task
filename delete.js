038a9402c34ff1bedb47d858faa6dda35df204c1
anishabhujbal3010 <anisha.bhujbal@searce.com>
2023-04-24 17:39:34 +0530

    Initial Commit

---
+++ delete.js
@@ -0,0 +1,13 @@
+// delete a todo
+app.delete('/todos/:id', (req, res) => {
+    const todos = JSON.parse(fs.readFileSync(todosFile));
+    const todoId = req.params.id;
+    const todoIndex = todos.findIndex(todo => todo.id === todoId);
+    if (todoIndex === -1) {
+      res.status(404).send('Todo not found');
+    } else {
+      todos.splice(todoIndex, 1);
+      fs.writeFileSync(todosFile, JSON.stringify(todos));
+      res.send(`Todo with id ${todoId} deleted`);
+    }
+  });
