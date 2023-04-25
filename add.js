3f8d39c43e63e154633d52b904f7d92c4ecd8b33
anishabhujbal3010 <anisha.bhujbal@searce.com>
2023-04-25 11:03:41 +0530

    Task cannot be empty

--- add.js
+++ add.js
@@ -2,6 +2,12 @@
 app.post('/todos', (req, res) => {
     const todos = JSON.parse(fs.readFileSync(todosFile));
     const newTodo = req.body;
+
+    if (!newTodo.task) {
+        res.status(400).send('Task cannot be empty');
+        return;
+      }
+      
     todos.push(newTodo);
     fs.writeFileSync(todosFile, JSON.stringify(todos));
     res.send(newTodo);
